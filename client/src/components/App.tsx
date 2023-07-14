5e34691dda1f11384a1aa59cc3c753cfb81f27ec
Jack Reichert <awesome@jackreichert.com>
2023-07-14 18:01:53 -0400

    intial graphql

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
 import '../App.scss';
+
+import React, { useEffect, useContext } from 'react';
+import axios from 'axios';
+
 import TaskInput from './TaskInput/index.tsx';
 import List from './TaskList/index.tsx';
 
+import AppContext from '../context.ts';
+
 function App() {
+  const { setTasks } = useContext(AppContext);
+
+  const query = `
+    query {
+      tasks {
+        title
+        status
+      }
+    }
+  `;
+
+  useEffect(() => {
+    axios.post('http://localhost:8000/graphql', { query })
+      .then((response) => setTasks(response.data.data.tasks));
+  }, [query, setTasks]);
+
   return (
     <>
       <h1 className="font-mono mb-4">Over-Engineered To Do App</h1>
---
+++ server/server/types.py
@@ -0,0 +1,16 @@
+type_defs = """
+    type Query {
+        tasks: [Task]
+    }
+
+
+    type Task {
+        title: String!
+        status: Boolean!
+    }
+"""
+
+tasks = [
+    {"title": "go to the store", "status": False},
+    {"title": "buy some cheese", "status": False}
+]
